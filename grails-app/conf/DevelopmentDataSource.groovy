4c4e61a470860ae2897e890093d989d9ffff190e
bobeal <bobeal@8b374472-8c1c-0410-9728-e5a9077ea544>
2006-09-17 18:35:47 +0000

    reaming due to Grails evolution about multiple data sources

--- grails-app/conf/DevelopmentDataSource.groovy
+++ grails-app/conf/DevelopmentDataSource.groovy
@@ -1,4 +1,4 @@
-class ApplicationDataSource {
+class DevelopmentDataSource {
    boolean pooling = true
    String dbCreate = "update" // one of 'create', 'create-drop','update'
    String url = "jdbc:postgresql://localhost:5432/poker"
