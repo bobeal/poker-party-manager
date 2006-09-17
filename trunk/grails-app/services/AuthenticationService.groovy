bb4a286cec36bc15e8e9a2fa33d4e94906d1c3b4
bobeal <bobeal@8b374472-8c1c-0410-9728-e5a9077ea544>
2006-09-17 18:36:57 +0000

    added an authentication service

---
+++ trunk/grails-app/services/AuthenticationService.groovy
@@ -0,0 +1,22 @@
+import java.security.MessageDigest
+import sun.misc.BASE64Encoder
+
+class AuthenticationService {
+    
+    def encryptPassword(clearPassword) {
+
+        def md = MessageDigest.getInstance("SHA-1")
+        def buf = clearPassword.getBytes()
+
+        md.update(buf)     //so we avoid password recovery
+        def hash = md.digest()     //hash of new password
+        if (hash.length <= 0) {    //no password was entered
+            println "encryptPassword() No password to hash"
+            return ""
+        }
+
+        def encryptedPwd = "{SHA}" + new BASE64Encoder().encode(hash)
+        println "encryptPassword() generated password : " + encryptedPwd
+        return encryptedPwd
+    }
+}
